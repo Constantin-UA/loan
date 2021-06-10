84fbe00cffeb25201ce1cb944f7423c5f077cea8
Kostiantyn <konstantinsh579@gmail.com>
2021-06-10 12:05:23 +0300

    add different module

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,42 @@
 import MainSlider from './modules/slider/slider-main';
 import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
+import Difference from './modules/difference';
 
 window.addEventListener('DOMContentLoaded', () => {
 	const slider = new MainSlider({ container: '.page', btns: '.next' });
 	slider.render();
 
-	const showUpSlider = new MiniSlider({
+	new MiniSlider({
 		container: '.showup__content-slider',
 		prev: '.showup__prev',
 		next: '.showup__next',
 		activeClass: 'card-active',
 		animate: true,
 		autoplay: true,
-	});
-	showUpSlider.init();
+	}).init();
 
-	const modulesSlider = new MiniSlider({
+	new MiniSlider({
 		container: '.modules__content-slider',
 		prev: '.modules__info-btns .slick-prev',
 		next: '.modules__info-btns .slick-next',
 		activeClass: 'card-active',
 		animate: true,
 		autoplay: true,
-	});
-	modulesSlider.init();
+	}).init();
 
-	const feedSlider = new MiniSlider({
+	new MiniSlider({
 		container: '.feed__slider',
 		prev: '.feed__slider .slick-prev',
 		next: '.feed__slider .slick-next',
 		activeClass: 'feed__item-active',
-	});
-	feedSlider.init();
+	}).init();
 
-	const player = new VideoPlayer('.showup .play', '.overlay');
-	player.init();
+	new VideoPlayer('.showup .play', '.overlay').init();
+
+	/* const difference = new Difference('.officerold', '.officernew', '.officer__card-item');
+	difference.init(); */
+
+	new Difference('.officerold', '.officer__card-item').init();
+	new Difference('.officernew', '.officer__card-item').init();
 });
---
+++ src/js/modules/difference.js
@@ -0,0 +1,37 @@
+export default class Difference {
+	constructor(officerSelector, items) {
+		this.useItems = document.querySelector(officerSelector).querySelectorAll(items);
+		this.useCounter = 0;
+	}
+
+	bindTriggers(container, counter) {
+		container.forEach((item) => {
+			if (item.querySelector('.plus')) {
+				item.addEventListener('click', () => {
+					container[counter].classList.add('fadeIn');
+					container[counter].classList.remove('fadeOut');
+					container[counter].style.display = 'flex';
+					if (counter !== container.length - 2) {
+						counter++;
+					} else {
+						container[container.length - 1].remove();
+					}
+				});
+			}
+		});
+	}
+	hideItems(items) {
+		items.forEach((item, i, arr) => {
+			if (i !== arr.length - 1) {
+				item.style.display = 'none';
+				item.classList.add('animated', 'fadeOut');
+				item.classList.remove('fadeIn');
+			}
+		});
+	}
+
+	init() {
+		this.hideItems(this.useItems);
+		this.bindTriggers(this.useItems, this.useCounter);
+	}
+}
