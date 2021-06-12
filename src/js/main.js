2c0ef8b64db09f28e6efcc56850901fa7a91aa7c
Kostiantyn <konstantinsh579@gmail.com>
2021-06-12 10:02:56 +0300

    add videoPlayer to the module page

--- src/js/main.js
+++ src/js/main.js
@@ -35,6 +35,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	}).init();
 
 	new VideoPlayer('.showup .play', '.overlay').init();
+	new VideoPlayer('.module__video-item .play', '.overlay').init();
 
 	new Difference('.officerold', '.officer__card-item').init();
 	new Difference('.officernew', '.officer__card-item').init();
